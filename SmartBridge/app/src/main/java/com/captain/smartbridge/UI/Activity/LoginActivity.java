236f7d1acd565a83b38709794460a16d03693c7e
moneyDboat <moneyboat@foxmail.com>
2017-04-24 18:14:55 +0800

    add Login page

--- SmartBridge/app/src/main/java/com/captain/smartbridge/UI/Activity/LoginActivity.java
+++ SmartBridge/app/src/main/java/com/captain/smartbridge/UI/Activity/LoginActivity.java
@@ -1,15 +1,19 @@
 package com.captain.smartbridge.UI.Activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.captain.smartbridge.R;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by fish on 17-4-24.
+ */
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
