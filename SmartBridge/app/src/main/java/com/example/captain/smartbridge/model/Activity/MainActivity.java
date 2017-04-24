554ffcddb3d605040047b4512d7cc1b75cb77782
moneyDboat <Captain@CaptaindeMacBook-Pro.local>
2017-04-24 15:40:32 +0800

    init

--- SmartBridge/app/src/main/java/com/example/captain/smartbridge/model/Activity/MainActivity.java
+++ SmartBridge/app/src/main/java/com/example/captain/smartbridge/model/Activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.captain.smartbridge;
+package com.example.captain.smartbridge.model.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.captain.smartbridge.R;
 
 public class MainActivity extends AppCompatActivity {
 
