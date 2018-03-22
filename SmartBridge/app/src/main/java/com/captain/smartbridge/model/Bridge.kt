ed2a600268faf250ab9bdc6f5fe90d447b10731f
Captain <moneyboat@foxmail.com>
2018-03-22 16:27:47 +0800

    update code

--- SmartBridge/app/src/main/java/com/captain/smartbridge/model/Bridge.kt
+++ SmartBridge/app/src/main/java/com/captain/smartbridge/model/Bridge.kt
@@ -1,15 +1,15 @@
 package com.captain.smartbridge.model
 
-import com.amap.api.maps2d.model.LatLng
+// import com.amap.api.maps2d.model.LatLng
 
 /**
  * Created by fish on 17-5-15.
  */
 
 class Bridge {
-    var latLng: LatLng? = null
-    var name: String? = null
-    var location: String? = null
-    var code: String? = null
-    var level: String? = null
+//    var latLng: LatLng? = null
+//    var name: String? = null
+//    var location: String? = null
+//    var code: String? = null
+//    var level: String? = null
 }
