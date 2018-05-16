f4639fc93fda3b5d7743a4abb3fd188b745a9a27
captain <moneyboat@foxmail.com>
2018-05-16 11:27:35 +0800

    add fly activity

--- SmartBridge/app/src/main/java/com/captain/smartbridge/UI/Activity/Check/Spec/FlyActivity.java
+++ SmartBridge/app/src/main/java/com/captain/smartbridge/UI/Activity/Check/Spec/FlyActivity.java
@@ -1,8 +1,46 @@
 package com.captain.smartbridge.UI.Activity.Check.Spec;
 
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.captain.smartbridge.R;
+import com.captain.smartbridge.UI.Activity.AbsActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * Created by captain on 18-5-15.
  */
 
-public class FlyActivity {
+public class FlyActivity extends AbsActivity{
+    @BindView(R.id.fly_toolbar)
+    Toolbar toolbar;
+
+    @Override
+    protected void setSelfContentView() {
+        setContentView(R.layout.activity_fly);
+    }
+
+    @Override
+    protected void prepareDatas() {
+
+    }
+
+    @Override
+    protected void initViews() {
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
--- SmartBridge/app/src/main/java/com/captain/smartbridge/UI/Fragment/PlaneFragment.java
+++ SmartBridge/app/src/main/java/com/captain/smartbridge/UI/Fragment/PlaneFragment.java
@@ -1,5 +1,6 @@
 package com.captain.smartbridge.UI.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -10,6 +11,7 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.captain.smartbridge.R;
+import com.captain.smartbridge.UI.Activity.Check.Spec.FlyActivity;
 import com.captain.smartbridge.UI.Adapters.other.PlaneListAdapter;
 import com.captain.smartbridge.model.Plane;
 
@@ -20,13 +22,13 @@ import java.util.List;
  * Created by captain on 18-5-15.
  */
 
-public class PlaneFragment extends Fragment{
+public class PlaneFragment extends Fragment {
     public static final String ARG_PAGE = "ARG_PAGE";
     public static final String ID = "";
     private int mPage;
     private View view;
 
-    public static PlaneFragment newInstance(int page){
+    public static PlaneFragment newInstance(int page) {
         Bundle args = new Bundle();
         args.putInt(ARG_PAGE, page);
         PlaneFragment fragment = new PlaneFragment();
@@ -54,34 +56,33 @@ public class PlaneFragment extends Fragment{
         }
     }
 
-    private View firstView(LayoutInflater inflater, ViewGroup container){
+    private View firstView(LayoutInflater inflater, ViewGroup container) {
         view = inflater.inflate(R.layout.fragment_plane1, container, false);
         return view;
     }
 
-    private View secView(LayoutInflater inflater, ViewGroup container){
+    private View secView(LayoutInflater inflater, ViewGroup container) {
         view = inflater.inflate(R.layout.fragment_plane2, container, false);
         ListView listView = (ListView) view.findViewById(R.id.plane2_list);
 
         //模拟数据
         List<Plane> planes = new ArrayList<>();
-        planes.add(new Plane("第一次飞行","2017-04-20"));
-        planes.add(new Plane("第二次飞行","2017-06-05"));
-        planes.add(new Plane("第三次飞行","2017-10-13"));
-        planes.add(new Plane("第四次飞行","2017-10-13"));
-        planes.add(new Plane("第五次飞行","2017-10-16"));
-        planes.add(new Plane("第六次飞行","2017-10-16"));
-        planes.add(new Plane("第七次飞行","2017-10-16"));
-        planes.add(new Plane("第八次飞行","2017-11-20"));
-        planes.add(new Plane("第九次飞行","2017-11-23"));
-        planes.add(new Plane("第十次飞行","2017-12-17"));
+        planes.add(new Plane("第一次飞行", "2017-04-20"));
+        planes.add(new Plane("第二次飞行", "2017-06-05"));
+        planes.add(new Plane("第三次飞行", "2017-10-13"));
+        planes.add(new Plane("第四次飞行", "2017-10-13"));
+        planes.add(new Plane("第五次飞行", "2017-10-16"));
+        planes.add(new Plane("第六次飞行", "2017-10-16"));
+        planes.add(new Plane("第七次飞行", "2017-10-16"));
+        planes.add(new Plane("第八次飞行", "2017-11-20"));
+        planes.add(new Plane("第九次飞行", "2017-11-23"));
+        planes.add(new Plane("第十次飞行", "2017-12-17"));
         PlaneListAdapter adapter = new PlaneListAdapter(getActivity(), planes);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                //列表点击事件
-                //跳转到飞行轨迹界面
+                startActivity(new Intent(getActivity(), FlyActivity.class));
             }
         });
 
